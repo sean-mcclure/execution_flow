61ae7d64a06f6f60051e3b31d1bc13f37e5274d3
Sean McClure <seanaaron100@gmail.com>
2020-10-18 21:48:34 +0000

    changing res to track_functs

--- execution_flow/tracker.py
+++ execution_flow/tracker.py
@@ -1,7 +1,7 @@
 import json
 
-res = {}
-res["children"] = []
+track_functs = {}
+track_functs["children"] = []
 
 class Tracker:
     
@@ -11,7 +11,7 @@ class Tracker:
         self.project_name = ""
         
     def project_name(self, project_name):
-        res["name"] = project_name
+        track_functs["name"] = project_name
 
     def __call__(self, fn):
         def wrapper(*args, **kwargs):
@@ -19,43 +19,43 @@ class Tracker:
                 inner = {}
                 inner["name"] = (fn.__name__)
                 inner["children"] = []
-                res["children"].append(inner)
+                track_functs["children"].append(inner)
             if(self.level == 1):
                 inner_b = {}
                 inner_b["name"] = (fn.__name__)
                 inner_b["children"] = []
-                res["children"][0]["children"].append(inner_b)
+                track_functs["children"][0]["children"].append(inner_b)
             if(self.level == 2):
                 inner_c = {}
                 inner_c["name"] = (fn.__name__)
                 inner_c["children"] = []
-                res["children"][0]["children"][0]["children"].append(inner_c)
+                track_functs["children"][0]["children"][0]["children"].append(inner_c)
             if(self.level == 3):
                 inner_d = {}
                 inner_d["name"] = (fn.__name__)
                 inner_d["children"] = []
-                res["children"][0]["children"][0]["children"][0]["children"].append(inner_d)
+                track_functs["children"][0]["children"][0]["children"][0]["children"].append(inner_d)
             if(self.level == 4):
                 inner_e = {}
                 inner_e["name"] = (fn.__name__)
                 inner_e["children"] = []
-                res["children"][0]["children"][0]["children"][0]["children"][0]["children"].append(inner_d)
+                track_functs["children"][0]["children"][0]["children"][0]["children"][0]["children"].append(inner_d)
             if(self.level == 5):
                 inner_f = {}
                 inner_f["name"] = (fn.__name__)
                 inner_f["children"] = []
-                res["children"][0]["children"][0]["children"][0]["children"][0]["children"][0]["children"].append(inner_d)
+                track_functs["children"][0]["children"][0]["children"][0]["children"][0]["children"][0]["children"].append(inner_d)
             if(self.level == 6):
                 inner_g = {}
                 inner_g["name"] = (fn.__name__)
                 inner_g["children"] = []
-                res["children"][0]["children"][0]["children"][0]["children"][0]["children"][0]["children"][0]["children"].append(inner_d)
+                track_functs["children"][0]["children"][0]["children"][0]["children"][0]["children"][0]["children"][0]["children"].append(inner_d)
             self.level += 1
             out = fn(*args, **kwargs)
             self.level -= 1
             
             with open('flow.json', 'w') as fp:
-                json.dump(res, fp, indent=2)
+                json.dump(track_functs, fp, indent=2)
             
             return out
         return wrapper
